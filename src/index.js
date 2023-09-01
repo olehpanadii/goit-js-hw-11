dc63ec3437c5a05f19944250560db4dc36232423
OlegPanadiy <opanadiy@gmail.com>
2023-09-01 21:33:54 +0100

    update 0109

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import Notiflix from 'notiflix';
 
 const elements = {
   form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
 };
 
 elements.form.addEventListener('submit', handlerrOnSubmit);
 
-function handlerrOnSubmit(evt) {
+async function handlerrOnSubmit(evt) {
   evt.preventDefault();
   const { searchQuery } = evt.currentTarget.elements;
 
@@ -31,5 +32,49 @@ function handlerrOnSubmit(evt) {
       );
     }
   }
-  fetchInfo();
+  try {
+    const data = await fetchInfo();
+    if (data.hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`);
+      elements.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+    }
+  } catch (error) {
+    Notiflix.Notify.failure('An error occurred while fetching data.');
+  }
+
+  function createMarkup(arr) {
+    return arr
+      .map(
+        ({
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>`
+      )
+      .join('');
+  }
 }
